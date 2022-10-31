5d6b17d0cbbf3ebf80704cf6f45f581698a5e708
importedgallo <andre.mongard@gmail.com>
2022-11-01 00:00:15 +1000

    syntax errors in colorscheme

--- lua/andre/core/colorscheme.lua
+++ lua/andre/core/colorscheme.lua
@@ -2,7 +2,7 @@ vim.cmd("colorscheme nightfly")
 
 local status, _ = pcall(vim.cmd, "colorscheme nightfly")
 if not status then
-  print("Colorscheme not found!"
+  print("Colorscheme not found!")
   return
 end
 

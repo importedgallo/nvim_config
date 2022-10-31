7a098056690b947df6c6cc654f33d81d59252753
importedgallo <andre.mongard@gmail.com>
2022-11-01 00:26:18 +1000

    changed colorscheme to tokyonight

--- lua/andre/core/colorscheme.lua
+++ lua/andre/core/colorscheme.lua
@@ -1,6 +1,6 @@
-vim.cmd("colorscheme nightfly")
+vim.cmd("colorscheme tokyonight")
 
-local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+local status, _ = pcall(vim.cmd, "colorscheme tokyonight")
 if not status then
   print("Colorscheme not found!")
   return

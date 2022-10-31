c6ff5c349a57d2306aed9f9a5f3a1fa933c5edf6
importedgallo <andre.mongard@gmail.com>
2022-11-01 00:37:25 +1000

    transparency

--- lua/andre/core/colorscheme.lua
+++ lua/andre/core/colorscheme.lua
@@ -1,8 +1,11 @@
-vim.cmd("colorscheme tokyonight")
-
 local status, _ = pcall(vim.cmd, "colorscheme tokyonight")
 if not status then
-  print("Colorscheme not found!")
-  return
+	print("Colorscheme not found!")
+	return
 end
 
+require("tokyonight").setup({
+	transparent = true,
+})
+
+vim.cmd("colorscheme tokyonight")

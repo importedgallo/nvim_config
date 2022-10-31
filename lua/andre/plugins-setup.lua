50df15dfada50278d02ef21f6f9e3854c651ca82
importedgallo <andre.mongard@gmail.com>
2022-10-31 23:57:34 +1000

    installed packer

--- lua/andre/plugins-setup.lua
+++ lua/andre/plugins-setup.lua
@@ -1,152 +1,105 @@
---config for packer plugins
+-- auto install packer if not installed
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+local packer_bootstrap = ensure_packer() -- true if packer was just installed
 
+-- autocommand that reloads neovim and installs/updates/removes plugins
+-- when file is saved
+vim.cmd([[ 
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
 
-return require('packer').startup(function()
+-- import packer safely
+local status, packer = pcall(require, "packer")
+if not status then
+	return
+end
 
---packer can manage itself
-        use 'wbthomason/packer.nvim'
+-- add list of plugins to install
+return packer.startup(function(use)
+	-- packer can manage itself
+	use("wbthomason/packer.nvim")
 
---colourscheme
-	use 'folke/tokyonight.nvim'
+	use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
 
-	require("tokyonight").setup({
-	  -- your configuration comes here
-	  -- or leave it empty to use the default settings
-	  style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-	  transparent = true, -- Enable this to disable setting the background color
-	  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-	  styles = {
-		    -- Style to be applied to different syntax groups
-		    -- Value is any valid attr-list value for `:help nvim_set_hl`
-		    comments = { italic = true },
-		    keywords = { italic = true },
-		    functions = {},
-		    variables = {},
-		    -- Background styles. Can be "dark", "transparent" or "normal"
-		    sidebars = "transparent", -- style for sidebars, see below
-		    floats = "transparent", -- style for floating windows
-		   },
-	  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-	  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-	  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-	  dim_inactive = false, -- dims inactive windows
-	  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+	use("bluz71/vim-nightfly-guicolors") -- preferred colorscheme
 
-	  --- You can override specific color groups to use other groups or a hex color
-	  --- function will be called with a ColorScheme table
-	  ---@param colors ColorScheme
-	  on_colors = function(colors) end,
+	use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
 
-	  --- You can override specific highlights to use other groups or a hex color
-	  --- function will be called with a Highlights and ColorScheme table
-	  ---@param highlights Highlights
-	  ---@param colors ColorScheme
-	  on_highlights = function(highlights, colors) end,
-	})
-	vim.cmd[[colorscheme tokyonight]]
+	use("szw/vim-maximizer") -- maximizes and restores current window
 
---lualine
-	use {
-	  'nvim-lualine/lualine.nvim',
-	  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-	}
-	require('lualine').setup {
-	  options = {
-	    icons_enabled = true,
-	    theme = 'tokyonight', 
-	    component_separators = { left = '', right = ''},
-	    section_separators = { left = '', right = ''},
-	    disabled_filetypes = {
-	      statusline = {},
-	      winbar = {},
-	    },
-	    ignore_focus = {},
-	    always_divide_middle = true,
-	    globalstatus = false,
-	    refresh = {
-	      statusline = 1000,
-	      tabline = 1000,
-	      winbar = 1000,
-	    }
-	  },
-	  sections = {
-	    lualine_a = {'mode'},
-	    lualine_b = {'branch', 'diff', 'diagnostics'},
-	    lualine_c = {'filename'},
-	    lualine_x = {'encoding', 'fileformat', 'filetype'},
-	    lualine_y = {'progress'},
-	    lualine_z = {'location'}
-	  },
-	  inactive_sections = {
-	    lualine_a = {},
-	    lualine_b = {},
-	    lualine_c = {'filename'},
-	    lualine_x = {'location'},
-	    lualine_y = {},
-	    lualine_z = {}
-	  },
-	  tabline = {},
-	  winbar = {},
-	  inactive_winbar = {},
-	  extensions = {}
-	}
+	-- essential plugins
+	use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
+	use("vim-scripts/ReplaceWithRegister") -- replace with register contents using motion (gr + motion)
 
---nvim-tree
-	use {
-	  'kyazdani42/nvim-tree.lua',
-	  requires = {
-	    'kyazdani42/nvim-web-devicons', -- optional, for file icons
-	  },
-	  tag = 'nightly' -- optional, updated every week. (see issue #1193)
-	}
+	-- commenting with gc
+	use("numToStr/Comment.nvim")
 
-	-- disable netrw at the very start of your init.lua (strongly advised)
-	vim.g.loaded = 1
-	vim.g.loaded_netrwPlugin = 1
+	-- file explorer
+	use("nvim-tree/nvim-tree.lua")
 
-	-- empty setup using defaults
-	require("nvim-tree").setup()
+	-- vs-code like icons
+	use("kyazdani42/nvim-web-devicons")
 
-	-- OR setup with some options
-	require("nvim-tree").setup({
-	  sort_by = "case_sensitive",
-	  view = {
-	    adaptive_size = true,
-	    mappings = {
-	      list = {
-		{ key = "u", action = "dir_up" },
-	      },
-	    },
-	  },
-	  renderer = {
-	    group_empty = true,
-	  },
-	  filters = {
-	    dotfiles = true,
-	  },
+	-- statusline
+	use("nvim-lualine/lualine.nvim")
+
+	-- fuzzy finding w/ telescope
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+
+	-- autocompletion
+	use("hrsh7th/nvim-cmp") -- completion plugin
+	use("hrsh7th/cmp-buffer") -- source for text in buffer
+	use("hrsh7th/cmp-path") -- source for file system paths
+
+	-- snippets
+	use("L3MON4D3/LuaSnip") -- snippet engine
+	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+	use("rafamadriz/friendly-snippets") -- useful snippets
+
+	-- managing & installing lsp servers, linters & formatters
+	use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
+	use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
+
+	-- configuring lsp servers
+	use("neovim/nvim-lspconfig") -- easily configure language servers
+	use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
+	use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
+	use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
+	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+
+	-- formatting & linting
+	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
+	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+
+	-- treesitter configuration
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
 	})
 
---autopairs
-	use {
-		"windwp/nvim-autopairs",
-	    config = function() require("nvim-autopairs").setup {} end
-	}
+	-- auto closing
+	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 
---treesitter
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-    }
+	-- git integration
+	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
---telescope
-    use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	-- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-	}
-
--- dashboard nvim
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
-
-
-

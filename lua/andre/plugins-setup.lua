17a47d4ce1c3434f14459b6518e5c6467532aa50
importedgallo <andre.mongard@gmail.com>
2022-11-03 18:49:59 +1000

    installed true zen

--- lua/andre/plugins-setup.lua
+++ lua/andre/plugins-setup.lua
@@ -61,6 +61,15 @@ return packer.startup(function(use)
 		end,
 	})
 
+	use({
+		"Pocco81/true-zen.nvim",
+		config = function()
+			require("true-zen").setup({
+				-- your config goes here
+				-- or just leave it empty :)
+			})
+		end,
+	})
 	-- statusline
 	use("nvim-lualine/lualine.nvim")
 

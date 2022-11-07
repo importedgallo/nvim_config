4df8fef80fb2e46bf0547aad9710b54de855f3eb
importedgallo <andre.mongard@gmail.com>
2022-11-08 00:50:39 +1000

    installed dashboard-nvim

--- lua/andre/plugins-setup.lua
+++ lua/andre/plugins-setup.lua
@@ -119,6 +119,9 @@ return packer.startup(function(use)
 	-- git integration
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
+	-- dashboard-nvim
+	use({ "glepnir/dashboard-nvim" })
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
